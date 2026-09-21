ad9fe823b4b9fb37f9ba752d2d21ac849cd2fe06
techFirm <ssegirinyapius07@gmail.com>
2026-09-21 21:14:05 +0300

    fix: serve Pikestia favicon

--- pikestia/urls.py
+++ pikestia/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +15,11 @@ urlpatterns = [
     path('', include('apps.opportunities.urls')),
     path('', include('apps.bookmarks.urls')),
     path('', include('apps.news.urls')),
+    path(
+        'favicon.ico',
+        RedirectView.as_view(url=f'{settings.STATIC_URL}favicon.svg', permanent=False),
+        name='favicon',
+    ),
 ]
 
 if settings.DEBUG:

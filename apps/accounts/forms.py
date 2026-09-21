bfaee49272f91cd64560ade64faee101969b861a
techFirm <ssegirinyapius07@gmail.com>
2026-09-21 09:17:02 +0300

    fix: add allauth signup profile field

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -12,3 +12,13 @@ class ThemeForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('display_mode',)
+
+
+class SignupExtraForm(forms.Form):
+    """Additional fields collected by the allauth signup flow."""
+
+    full_name = forms.CharField(max_length=150, required=True)
+
+    def signup(self, request, user):
+        user.full_name = self.cleaned_data['full_name']
+        user.save(update_fields=['full_name'])
